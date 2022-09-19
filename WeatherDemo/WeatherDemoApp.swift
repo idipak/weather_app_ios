04c2685900c19e154e9b3c3382a2a8b2bd55fde4
Dipak <dipak_pd@outlook.com>
2022-09-19 14:08:57 +0530

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Codebucket on 19/09/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
